403d563e6b4bdbabd46d8358cd377dd436d2b998
Alberto~Ferioli <alberto@mumsnet.com>
2017-04-06 23:19:15 +0100

    render threads to the app

--- src/components/TopicList.js
+++ src/components/TopicList.js
@@ -3,21 +3,28 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class TopicList extends Component {
+  state = {
+    threads: []
+  };
+
   componentWillMount() {
     axios.get('https://stageapi6.devmn.net/api/v1/forums/threads?topic_id=2418', {
       headers: {
         'client-id': 'e2c2cbcfb116aef33d4bfad38a6ac73e8bd1c064305264d8cbbf133ebfa0f21e'
       }
     })
-      .then(response => console.log(response.data.threads.map(
-        (thread) => console.log(thread.thread.name)
-      )));
+      .then(response => this.setState({ threads: response.data.threads }));
+  }
+
+  renderThreads() {
+    return this.state.threads.map(thread => <Text>{thread.thread.name}</Text>);
   }
 
   render() {
+
     return (
       <View>
-        <Text>TopicList</Text>
+        {this.renderThreads()}
       </View>
     );
   }
